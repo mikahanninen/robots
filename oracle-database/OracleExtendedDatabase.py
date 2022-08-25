c94c7f9dd41507052a4d1757295ebe0b99f121ca
Mika Hänninen <mika.hanninen@gmail.com>
2022-08-25 14:24:42 +0300

    add keyword doc

--- oracle-database/OracleExtendedDatabase.py
+++ oracle-database/OracleExtendedDatabase.py
@@ -3,18 +3,29 @@ from pydoc import locate
 
 
 class OracleExtendedDatabase(Database):
+    """This library extends keywords of the parent `RPA.Database` library.
+    
+    New keyword provided: Oracle Call Procedure
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def oracle_call_procedure(
         self, procedure_name: str, input_parameters: list, result_type: str = "str"
     ):
-        """_summary_
+        """Keyword for calling a stored procedure in the Oracle Database.
 
-        :param procedure_name: _description_
-        :param input_parameters: _description_
-        :param result_type: _description_
-        :return: _description_
+        :param procedure_name: name of the stored procedure
+        :param input_parameters: all procedure parameters in a list
+        :param result_type: type of the return value (default 'str')
+        :return: returnable value of the stored procedure result
+
+        Example:
+
+        .. code-block:: robotframework
+
+            @{params}     Create List   FirstParam   SecondParam   ThirdParam
+            ${result}     Call Stored Procedure   mystpr  ${params}
         """
         cursor = self._dbconnection.cursor()
         result_object = cursor.var(locate(result_type))
